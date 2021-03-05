1d274a6a111fc02f3ee27b883066aa0e77545d59
victorhugo <victorblog410@gmail.com>
2021-03-05 12:26:37 -0300

    settings host

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 
-from decouple import config
+from decouple import config, Csv
 from pathlib import Path
 from dj_database_url import parse as dburl
 
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 # Application definition
 
